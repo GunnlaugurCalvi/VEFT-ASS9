a07dbdae0fc705ae34479f2c24ece87b425d6708
Gunnlaugur Hreiðarsson <Gunnlaugur Hreiarsson>
2017-10-21 20:32:32 +0000

    add function tested :lollipop:

--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,22 @@
+const {add} = require("./index");
+
 describe('Remove me', () => {
   test('Remove this', () => {
-    expect(1).toBe(1);
+   expect(1).toBe(1);
   });
+	describe('add', () =>{
+		test('Should successfully add two integers together', () => {
+			expect(add(1,1)).toBe(2);
+		});
+	});
+	describe('add', () => {
+		test('should return just 1', () => {
+			expect(add(null, 1)).toBe(1);
+		});
+	});
+	describe('add', () => {
+		test('nothing is entered returns 0', () => {
+			expect(add()).toEqual(NaN);
+		});
+	});
 });
