072b1776551f1be5c52b2fe496bb1f2256e6f800
Gunnlaugur Hreiðarsson <Gunnlaugur Hreiarsson>
2017-10-21 23:23:26 +0000

    2.automock

--- assignment.test.js
+++ assignment.test.js
@@ -1,22 +1,27 @@
-const {add} = require("./index");
+const {add, throws} = require("./index");
+//import * as th from './index';
 
-describe('Remove me', () => {
-  test('Remove this', () => {
-   expect(1).toBe(1);
-  });
-	describe('add', () =>{
-		test('Should successfully add two integers together', () => {
-			expect(add(1,1)).toBe(2);
-		});
+describe('add', () =>{
+	test('Should successfully add two integers together', () => {
+		expect(add(1,1)).toBe(2);
 	});
-	describe('add', () => {
-		test('should return just 1', () => {
-			expect(add(null, 1)).toBe(1);
-		});
+	test('should return just 1', () => {
+		expect(add(null, 1)).toBe(1);
 	});
-	describe('add', () => {
-		test('nothing is entered returns 0', () => {
-			expect(add()).toEqual(NaN);
-		});
+	test('nothing is entered returns 0', () => {
+		expect(add()).toEqual(NaN);
 	});
+});
+describe('throws', () => {
+	test('Should not throw error', () => {
+		const callSum = () => {
+			throws(true);
+		};
+	expect(callSum).toThrowError('You need to mock me');
+	});
+/*	test('should test if it returns false', () => {
+		throws(true,() => {
+			expect(true).toThrowError('You need to mock me');
+		});	
+	});	*/
 });
